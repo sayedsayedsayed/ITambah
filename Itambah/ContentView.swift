ff1b262b8029d4362283d597f57b528ca7d94453
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-08-10 16:48:56 +0700

    initial commit

---
+++ Itambah/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Itambah
+//
+//  Created by Vicky Irwanto on 03/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
